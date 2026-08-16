68f3f3f6ddf9859e740b166c426aafa04f3d94ea
Vicente Zapata <vicentezapatac@gmail.com>
2026-08-16 17:09:50 -0400

    Refactor skill 2

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import next from 'eslint-config-next/core-web-vitals';
-
-const eslintConfig = [
-  { ignores: ['.next/**', 'node_modules/**', 'cursos/**', '.agents/**', 'public/**'] },
-  ...(Array.isArray(next) ? next : [next]),
-];
-
-export default eslintConfig;
